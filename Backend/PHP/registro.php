b84bc4b5dc406b385d7ab49c3f74444d56d1bcb5
Jesús Domínguez Ramírez <230028@utxicotepec.edu.mx>
2024-07-27 21:59:59 -0600

    Actualización de archivo en carpeta de PHP, en Backend.
    Se actualizo el archivo "registro.php", con un código ya funcional.

--- Backend/PHP/registro.php
+++ Backend/PHP/registro.php
@@ -1,15 +1,15 @@
 <?php 
-include("/Backend/DB_Connection/db_connection.php");
+include("C:/xampp/htdocs/Atest/Backend/DB_Connection/db_connection.php");
 if (isset($_POST['registrarb'])) { /*Si es que se presiono el boton registrarb...*/
-    if (strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1) { /*Y si la longitud de lo que esta en los campos de 'name' y 'email' es mayor o igual a 1 (basicamente confirma que tiene algo dentro)*/
+    if (strlen($_POST['username']) >= 1 && strlen($_POST['email']) >= 1) { /*Y si la longitud de lo que esta en los campos de 'name' y 'email' es mayor o igual a 1 (basicamente confirma que tiene algo dentro)*/
 		$namet = trim($_POST['username']);
-		$email = trim(&_POST['email']);
-		$fechareg = date("d/m/y");
-        $consulta = "INSERT INTO datos(nombre, email, fecha_reg) VALUES ('$name','$email','$fechareg')" 
+		$email = trim($_POST['email']);
+		$fechareg = date("y/m/d");
+        $consulta = "INSERT INTO datos(nombre, email, fecha_reg) VALUES ('$namet','$email','$fechareg')";
 		$resultado = mysqli_query($conn,$consulta);
 	    if ($resultado) {
 	    	?> 
-	    	<h3 class="ok">¡Registro completado!</h3>
+	    	<h3 class="bad">¡Registro completado!</h3>
            <?php
 	        } else {
 	    	?> 
