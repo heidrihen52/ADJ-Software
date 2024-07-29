ffa5f35f854a6af51fb0c5ac4d1320750f655e68
Jesús Domínguez Ramírez <230028@utxicotepec.edu.mx>
2024-07-28 20:25:24 -0600

    Nuevo archivo en Scripts
    Se agrego el archivo "usuarios_db.sql" en la subcarpeta Scripts de Databases.

---
+++ Databases/Scripts/usuarios_db.sql
@@ -0,0 +1,14 @@
+create table usuarios_db (
+	id_usuario int primary key AUTO_INCREMENT,
+	nombre varchar(25),
+  nickname varchar(50)
+	email varchar(50),
+	contraseña varchar(25), 
+	fecha_nac date,
+	edad int date,
+	fecha_reg date,
+	n_telefono int,
+	altura int,
+	peso decimal(10,2),
+	rol enum(usuario,administrador)
+);
