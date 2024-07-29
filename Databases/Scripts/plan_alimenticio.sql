52f50320e975336ef462d144d8b1d725b664b136
Jesús Domínguez Ramírez <230028@utxicotepec.edu.mx>
2024-07-28 20:49:07 -0600

    Actualización de archivo "plan_alimenticio.sql" en Scripts
    Se agrego una nueva columna a "plan_alimenticio.sql" dentro de Scripts en Databases (se agrego la foreign key id_usuario).

--- Databases/Scripts/plan_alimenticio.sql
+++ Databases/Scripts/plan_alimenticio.sql
@@ -1,5 +1,6 @@
 create table plan_alimenticio (
 	id_plan int not null primary key AUTO_INCREMENT,
+	id_usuario int FOREIGN KEY REFERENCES usuarios_db(id_usuario),
 	nombre_plan varchar(100)
 	duracion int,
 	categoria enum(ganancia_muscular,perdida_grasa,acondicionamiento)
