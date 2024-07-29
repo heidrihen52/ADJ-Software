cf3739727187981f3720abae49a300fac392b01b
Jesús Domínguez Ramírez <230028@utxicotepec.edu.mx>
2024-07-28 20:49:04 -0600

    Actualización de archivo "rutinas.sql" en Scripts
    Se agrego una nueva columna a "rutinas.sql" dentro de Scripts en Databases (se agrego la foreign key id_usuario).

--- Databases/Scripts/rutinas.sql
+++ Databases/Scripts/rutinas.sql
@@ -1,5 +1,6 @@
 create table rutinas (
 	id_rutina int not null primary key AUTO_INCREMENT,
+	id_usuario int FOREIGN KEY REFERENCES usuarios_db(id_usuario),
 	nombre_rutina varchar(100),
 	categoria enum(ganancia_muscular,perdida_grasa,acondicionamiento),
 	dificultad enum(principiante,intermedio,avanzado),
