30ffe165bb24ee5f6a51032ba886ca693726b712
Jesús Domínguez Ramírez <230028@utxicotepec.edu.mx>
2024-08-05 13:30:46 -0600

    Actualización de Archivo 'rutinas.sql'
    Se arreglaron errores de sintaxis del archivo 'rutinas.sql' en Databases y Scripts.

--- Databases/Scripts/rutinas.sql
+++ Databases/Scripts/rutinas.sql
@@ -1,8 +1,10 @@
 create table rutinas (
 	id_rutina int not null primary key AUTO_INCREMENT,
-	id_usuario int FOREIGN KEY REFERENCES usuarios_db(id_usuario),
+	id_usuario int,
 	nombre_rutina varchar(100),
-	categoria enum(ganancia_muscular,perdida_grasa,acondicionamiento),
-	dificultad enum(principiante,intermedio,avanzado),
-	complemento_nutricional int null foreign key references plan_alimenticio(id_plan)
+	categoria enum('ganancia_muscular','perdida_grasa','acondicionamiento'),
+	dificultad enum('principiante','intermedio','avanzado'),
+	id_plan int null,
+    foreign key (id_plan) references plan_alimenticio(id_plan),
+    foreign key (id_usuario) references usuarios_db(id_usuario)
 );
