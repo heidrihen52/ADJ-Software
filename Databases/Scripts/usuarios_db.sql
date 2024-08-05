90097948abb4c9c1e5c8928a0e1d487646757b76
Jesús Domínguez Ramírez <230028@utxicotepec.edu.mx>
2024-08-05 13:30:54 -0600

    Actualización de Archivo 'usuarios_db'
    Se arreglaron errores de sintaxis del archivo 'usuarios_db' en Databases y Scripts.

--- Databases/Scripts/usuarios_db.sql
+++ Databases/Scripts/usuarios_db.sql
@@ -1,15 +1,15 @@
 create table usuarios_db (
 	id_usuario int primary key AUTO_INCREMENT,
 	nombre varchar(25),
-	nickname varchar(50)
+	nickname varchar(50),
 	email varchar(50),
 	contraseña varchar(25), 
 	fecha_nac date,
-	edad int date,
+	edad int,
 	fecha_reg date,
 	n_telefono int,
 	altura int,
 	peso decimal(10,2),
 	emblemas int,
-	rol enum(usuario,administrador)
+	rol enum('usuario','administrador')
 );
