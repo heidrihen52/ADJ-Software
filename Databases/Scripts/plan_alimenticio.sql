82bbf32976da159eaa39c615fa64308dc49cf070
Jesús Domínguez Ramírez <230028@utxicotepec.edu.mx>
2024-08-05 13:30:58 -0600

    Actualización de Archivo 'plan_alimenticio'
    Se arreglaron errores de sintaxis del archivo 'plan_alimenticio' en Databases y Scripts.

--- Databases/Scripts/plan_alimenticio.sql
+++ Databases/Scripts/plan_alimenticio.sql
@@ -1,7 +1,8 @@
 create table plan_alimenticio (
 	id_plan int not null primary key AUTO_INCREMENT,
-	id_usuario int FOREIGN KEY REFERENCES usuarios_db(id_usuario),
-	nombre_plan varchar(100)
+	id_usuario int,
+	nombre_plan varchar(100),
 	duracion int,
-	categoria enum(ganancia_muscular,perdida_grasa,acondicionamiento)
+	categoria enum('ganancia_muscular','perdida_grasa','acondicionamiento'),
+    FOREIGN KEY (id_usuario) references usuarios_db(id_usuario)
 );
