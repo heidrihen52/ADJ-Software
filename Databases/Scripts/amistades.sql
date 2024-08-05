1becf707e644b7eda8f0c0190131d49caa1a6481
Jesús Domínguez Ramírez <230028@utxicotepec.edu.mx>
2024-08-05 13:30:59 -0600

    Actualización de Archivo 'amistades'
    Se arreglaron errores de sintaxis del archivo 'amistades' en Databases y Scripts..

--- Databases/Scripts/amistades.sql
+++ Databases/Scripts/amistades.sql
@@ -1,6 +1,8 @@
 create table amistades (
   amistad_id int not null primary key auto_increment,
-  usuario1_id int foreign key references usuarios_db(id_usuario),
-  amigo_id int foreign key references usuarios_db(id_usuario),
-  estatus enum('activo','pendiente')
+  usuario1_id int,
+  amigo_id int,
+  estatus enum('activo','pendiente'),
+  foreign key (usuario1_id) references usuarios_db(id_usuario),
+  foreign key (amigo_id) references usuarios_db(id_usuario)
 );
