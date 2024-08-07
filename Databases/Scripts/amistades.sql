ad4e0369905956911bebfc5fa6af101b3e4aa1e5
Jesús Domínguez Ramírez <230028@utxicotepec.edu.mx>
2024-08-06 20:47:01 -0600

    Se elimino un archivo de Scripts
    Se elimino el archivo 'amistades.sql' de la subcarpeta Scripts, en Databases.

--- Databases/Scripts/amistades.sql
+++
@@ -1,8 +0,0 @@
-create table amistades (
-  amistad_id int not null primary key auto_increment,
-  usuario1_id int,
-  amigo_id int,
-  estatus enum('activo','pendiente'),
-  foreign key (usuario1_id) references usuarios_db(id_usuario),
-  foreign key (amigo_id) references usuarios_db(id_usuario)
-);
